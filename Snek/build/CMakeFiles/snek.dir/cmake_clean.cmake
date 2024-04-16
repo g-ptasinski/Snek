b1236f8e402e57f6e59f31ae73f6b54cb7f30863
g.ptasinski <g.ptasinski@ai.com.pl>
2024-04-16 21:41:40 +0200

    Initial commit, skeleton

---
+++ Snek/build/CMakeFiles/snek.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snek.dir/main.cpp.o"
+  "CMakeFiles/snek.dir/main.cpp.o.d"
+  "snek"
+  "snek.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
