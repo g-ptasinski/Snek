bbe3612d5cbe6afd7c528011048130b99ad04da4
ptasinski.grzegorz <grzesioo66@gmail.com>
2024-05-15 23:26:47 +0200

    Added walls and food

--- Snek/src/gameplay.cpp
+++ Snek/src/gameplay.cpp
@@ -18,6 +18,24 @@ void Gameplay::Init()
 
     m_grass.setTexture(m_context->m_assets->GetTexture(GRASS));
     m_grass.setTextureRect(m_context->m_window->getViewport(m_context->m_window->getDefaultView()));
+
+    m_food.setTexture(m_context->m_assets->GetTexture(FOOD));
+    m_food.setPosition(m_context->m_window->getSize().x/2,m_context->m_window->getSize().y/2);
+
+    for (auto& wall : m_walls)
+    {
+        wall.setTexture(m_context->m_assets->GetTexture(WALL));
+    }
+
+    int WallWidth = 16;
+
+    m_walls[0].setTextureRect({0,0,     m_context->m_window->getSize().x,WallWidth});
+    m_walls[1].setTextureRect({0,0,     WallWidth,m_context->m_window->getSize().y});
+    m_walls[2].setTextureRect({0,0,     m_context->m_window->getSize().x,WallWidth});
+    m_walls[3].setTextureRect({0,0,     WallWidth,m_context->m_window->getSize().y});
+
+    m_walls[2].setPosition(0, static_cast<float>(m_context->m_window->getSize().y - WallWidth));
+    m_walls[3].setPosition(static_cast<float>(m_context->m_window->getSize().x - WallWidth), 0);
 }
 
 void Gameplay::Update(sf::Time deltaTime)
@@ -28,12 +46,29 @@ void Gameplay::Update(sf::Time deltaTime)
 
 void Gameplay::ProcessInput()
 {
+    sf::Event event;
+
+    while(m_context->m_window->pollEvent(event))
+    {
+        if(event.type == sf::Event::Closed)
+        {
+            m_context->m_window->close();
+        }
+    }
 }
 
 void Gameplay::Draw()
 {
     m_context->m_window->clear();
+
     m_context->m_window->draw(m_grass);
+
+    for (auto& wall : m_walls)
+    {
+        m_context->m_window->draw(wall);
+    }
+
+    m_context->m_window->draw(m_food);
     m_context->m_window->display();
 }
 
